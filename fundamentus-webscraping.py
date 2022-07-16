195f679a4949c9d9a368585431ba4f9c5499af51
Paulo Feitosa <paulovnfeitosa@gmail.com>
2022-07-16 15:10:51 -0300

    Adicionado coleta de dados em forma de Dataframe

--- fundamentus-webscraping.py
+++ fundamentus-webscraping.py
@@ -2,12 +2,11 @@ from urllib import response
 from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+import pandas as pd
 
 def trataHTML(input):
-    #Decodificar
-    input=input.decode('ISO-8859-1')
-    #Transformar html em texto contínuo sem espaços
-    return " ".join(input.split()).replace('> <', '><')
+    input=input.decode('ISO-8859-1')                        #Decodificar
+    return " ".join(input.split()).replace('> <', '><')     #Transformar html em texto contínuo sem espaços
 
 def requisitaURL(url, headers):
     try:
@@ -21,17 +20,31 @@ def requisitaURL(url, headers):
         print(e.reason)
     return trataHTML(html)
 
+def coletaInfo(html):
+    soup =  BeautifulSoup(html, 'html.parser')                  #Interpretador HTML
+    colunas=[item.getText() for item in soup.find_all('th')]    #Coletando nome das colunas
+    infoAtivos=pd.DataFrame(columns=colunas)                    #Criando dataFrame vazio
+    
+    k=0
+    for item in soup.find_all('tr'):                            #Lendo e adicionando linha a linha
+        if k!=0:
+            infoAtivos.loc[-1] = [i.getText() for i in item.find_all('td')]  
+            infoAtivos.index = infoAtivos.index + 1  
+            infoAtivos = infoAtivos.sort_index()
+        k+=1
+    return infoAtivos
+
 urlAcoes ='https://www.fundamentus.com.br/resultado.php'
-urlFii = 'https://www.fundamentus.com.br/fii_resultado.php'
+urlFiis = 'https://www.fundamentus.com.br/fii_resultado.php'
 headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36'}
 
 htmlAcoes = requisitaURL(urlAcoes, headers)
-htmlFii = requisitaURL(urlFii, headers)
+htmlFiis = requisitaURL(urlFiis, headers)
 
-soup =  BeautifulSoup(htmlAcoes, 'html.parser')
 
-for item in soup.find_all('tr'):
-    for i in item.find_all('td'):
-        print(i.getText())
-        input()
-    #print(item.find('p', class_='txt-value').getText())
+acoes = coletaInfo(htmlAcoes)
+fiis = coletaInfo(htmlFiis)
+
+print(acoes)
+print(fiis)
+
